85fca64b11b0048c5dd2f2a947659be41e55bea4
chat2004 <huynhminhvu@gmail.com>
2018-05-23 15:02:42 +0700

    Add annotations project

--- spring-demo-1/src/com/love2code/springdemo/TrackCoach.java
+++ spring-demo-1/src/com/love2code/springdemo/TrackCoach.java
@@ -23,4 +23,13 @@ public class TrackCoach implements Coach {
 		return "Just do it: " + fortuneService.getFortune();
 	}
 
+	// add init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add destroy method
+	public void doMyCleanupStuff() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuff");
+	}
 }

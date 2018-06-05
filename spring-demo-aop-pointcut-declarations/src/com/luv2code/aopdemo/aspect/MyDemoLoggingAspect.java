8a2c3ac69a1b5fb84789f8d77d5f7b5dec0b88d8
chat2004 <huynhminhvu@gmail.com>
2018-06-05 22:15:56 +0700

    add new project

--- spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,11 +2,16 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+	
+	// define a point cut
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
 	
 	// this is where we add all of our related advice for logging
 	
@@ -14,8 +19,15 @@ public class MyDemoLoggingAspect {
 	
 //	@Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
 	//@Before("execution(public void add*())")
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	//@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n========> Executing @Before advice on method addAccount() ");
 	}
+	
+	// add new advice
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n========> Perform API Analytics method");
+	}	
 }

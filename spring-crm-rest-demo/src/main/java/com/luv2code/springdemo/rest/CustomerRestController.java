221130c3acb636ad566d1f077dfa717bb507554f
chat2004 <huynhminhvu@gmail.com>
2018-06-12 06:23:11 +0700

    test

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo.rest;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +46,18 @@ public class CustomerRestController {
 		customerService.saveCustomer(theCustomer);
 		
 		return theCustomer;
+	}
+	
+	// add maping for PUT /customers, update customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		customerService.saveCustomer(theCustomer);
+		return theCustomer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public List<Customer> deleteCustomer(@PathVariable int customerId) {
+		customerService.deleteCustomer(customerId);
+		return getCustomers();
 	}
 }

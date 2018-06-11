b990997a2298533f5a6aae080b2b47bdba5fae0f
chat2004 <huynhminhvu@gmail.com>
2018-06-11 16:57:06 +0700

    add REST projects

---
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -0,0 +1,54 @@
+package com.luv2code.springdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.PostConstruct;
+
+import com.luv2code.springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+	private List<Student> theStudents;
+	
+	// define @PostContructor to load the student data
+	@PostConstruct
+	public void loadData() {
+		
+		theStudents = new ArrayList<>();
+		theStudents.add(new Student("Huynh", "Vu"));
+		theStudents.add(new Student("Duong", "Ngoc"));
+		theStudents.add(new Student("Huynh", "Khoi"));
+		
+	}
+	
+	// define endpoint for get student list
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		
+		return theStudents;
+	}
+	
+	// define endpoit for "/students/{studentId} - return a student at index
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+
+		// check for student id
+		if ((studentId >= theStudents.size()) || (studentId < 0)) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+		// return the student 
+		return theStudents.get(studentId);
+	}
+	
+	
+}
